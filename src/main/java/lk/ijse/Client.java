e520052ed0586a0387b0da5ca759233c79a49e68
LRLayan <rameshlayan4@gmail.com>
2024-01-06 02:33:32 +0530

    create client class and method

--- src/main/java/lk/ijse/Client.java
+++ src/main/java/lk/ijse/Client.java
@@ -1,2 +1,38 @@
-package lk.ijse;public class Client {
+package lk.ijse;
+
+import java.io.*;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("localhost" , 3000);
+
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+            System.out.println("Start Chat With Server : ");
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+            String message = "";
+            String reply = "";
+
+            while (!message.equals("finish")){
+              //  System.out.println("me : ");
+                reply = bufferedReader.readLine();
+
+                dataOutputStream.writeUTF(reply);
+                dataOutputStream.flush();
+
+                message = dataInputStream.readUTF();
+                System.out.println("Server response : " + message);
+            }
+            dataInputStream.close();
+            dataOutputStream.close();
+            bufferedReader.close();
+            socket.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
