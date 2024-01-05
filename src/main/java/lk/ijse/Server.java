45c3ff7c1c5985f1f26085b3b43230060edf6c67
LRLayan <rameshlayan4@gmail.com>
2024-01-06 02:33:48 +0530

    create server class and method

--- src/main/java/lk/ijse/Server.java
+++ src/main/java/lk/ijse/Server.java
@@ -1,2 +1,47 @@
-package lk.ijse;public class Server {
+package lk.ijse;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args)  {
+        try {
+            //Main server socket
+            ServerSocket serverSocket = new ServerSocket(3000);
+            //Local server socket
+            Socket socket = serverSocket.accept();
+            //Client Request Accepted!
+            System.out.println("Accepted Request!");
+
+            //server output message send client
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            //Type server message input
+            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+            System.out.println("Enter the response for client");
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+            String message = "me :";
+            String reply = "";
+
+            while (!message.equals("finish")){
+                //Client Request message
+                message = dataInputStream.readUTF();
+                System.out.println("Client : " + message);
+                //Server reply for client
+                reply = bufferedReader.readLine();
+
+                dataOutputStream.writeUTF(reply);
+                dataOutputStream.flush();
+            }
+
+            dataInputStream.close();
+            dataOutputStream.close();
+            bufferedReader.close();
+            socket.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
